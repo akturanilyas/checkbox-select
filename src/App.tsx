bb1f6ac83e901667138efbcc4b706f86c46cb582
Ilyas AKTURAN <akturanilyas@gmail.com>
2023-11-22 22:08:40 +0300

    Update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import './App.css';
 import { Home } from './pages/Home.tsx';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import BaseView from './components/common/base-view/BaseView.tsx';
 
 const queryClient = new QueryClient();
 
-function App() {
-  return (
+const App = () => (
+  <BaseView>
     <QueryClientProvider client={queryClient}>
       <Home />
     </QueryClientProvider>
-  );
-}
+  </BaseView>
+);
 
 export default App;
